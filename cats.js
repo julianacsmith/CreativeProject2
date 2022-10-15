82776a25ad0d3db0d4bf73d86ddfca56a1561dfd
julianacsmith <juliana.c.smith20@gmail.com>
2022-10-15 06:34:27 +0000

    Some CSS and HTML Work

--- cats.js
+++ cats.js
@@ -1,9 +1,5 @@
 document.getElementById("catSubmit").addEventListener("click", function(event) {
   event.preventDefault();
-  //const value = document.getElementById("weatherInput").value;
-  //if (value === "")
-  //  return;
-  //console.log(value);
   
     const url = "https://api.thecatapi.com/v1/images/search?api_key=live_LNT8fQmDiJdFD8iRCG4CUoPltcFhAs95Zuk7QH6FWd3iblUrcSVCdUxhcyykXR2r";
     /*global fetch*/
@@ -25,7 +21,7 @@ document.getElementById("catSubmit").addEventListener("click", function(event) {
         return response.json();
       }).then(function(json){
         console.log(json[0].quote);
-        let result = json[0].quote + "\n\t-" + json[0].author;
+        let result = json[0].quote + "<br>-" + json[0].author;
         document.getElementById("quote").innerHTML = result;
       });
 });
